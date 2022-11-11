cf43cd04d0f9ab943b154c5d54dd919fa589c0cb
Zübeyde Ulukök <100999601+zubeydeulukok@users.noreply.github.com>
2022-11-11 22:59:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,16 @@ import matplotlib.pyplot as plt
 
 
 st.set_page_config(page_title="Credit Card Fraud Detection App")
-import zipfile
-with zipfile.ZipFile("Final_data.zip","r") as zip_ref:
-    zip_ref.extractall()
-    
+# import zipfile
+# with zipfile.ZipFile("Final_data.zip","r") as zip_ref:
+#     zip_ref.extractall()
+
+unzipped_file = zipfile.ZipFile("Final_data.zip", "r") # get contents without extracting.
+df = unzipped_file.read("Final_data.csv")
+
 #Import the dataset
-fraud = pd.read_csv("https://github.com/zubeydeulukok/Fraud_Detection/edit/main/Final_data.csv")
-df = fraud.copy()
+# fraud = pd.read_csv("https://github.com/zubeydeulukok/Fraud_Detection/edit/main/Final_data.csv")
+# df = fraud.copy()
 csv = df.to_csv(index=False)
 b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
 
